fc10ba4ac867db494d8185d04b982249a96d1b80
Briam <briammartinez@gmail.com>
2019-12-24 13:49:26 +0100

    Added Icon component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <nav>
       <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <px-icon class="mr-2" />
+          <Icon class="mr-2" />
           <span class="font-semibold text-xl tracking-tight">PlatziExchange</span>
         </div>
         <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
@@ -13,3 +13,14 @@
     </nav>
   </header>
 </template>
+
+<script>
+import Icon from '@/components/Icon';
+
+export default {
+  name: 'Header',
+  components: {
+    Icon
+  }
+};
+</script>
