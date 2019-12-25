cd8b20c65f043fa7cd8a3d7dbd482813ce2cfa70
Briam <briammartinez@gmail.com>
2019-12-26 00:03:29 +0100

    Created inital version of about view

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Currency Exchange App</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Project to obtain the quotations of the most important cryptomontages through Coincap's API REST.
+    </p>
+
+    <router-link
+      to="/"
+      class="mt-5 text-xl text-green-600 hover:underline">
+      Back to home page
+    </router-link>
+  </div>
+</template>
