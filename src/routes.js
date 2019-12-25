3aab218202f0f2162a0f61201302e023b5d356fd
Briam <briammartinez@gmail.com>
2019-12-26 00:02:24 +0100

    Added routes of about and error

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '@/views/Home';
+import About from '@/views/About';
+import ErrorView from '@/views/ErrorView';
 
 Vue.use(Router);
 
@@ -13,6 +15,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+    {
+      path: '*',
+      name: 'error',
+      component: ErrorView,
     },
   ],
 });
