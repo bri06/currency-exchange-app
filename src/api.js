718d9cd09f6219a2b3cc39779f020e4cf470c535
Briam <briammartinez@gmail.com>
2019-12-27 13:37:46 +0100

    Added getAsset and getAssetHistory methods into the api

--- src/api.js
+++ src/api.js
@@ -4,3 +4,17 @@ const URL = 'https://api.coincap.io/v2';
 export const getAssets = () => fetch(`${URL}/assets?limit=20`)
   .then(res => res.json())
   .then(({ data }) => data);
+
+export const getAsset = id => fetch(`${URL}/assets/${id}`)
+  .then(res => res.json())
+  .then(({ data }) => data);
+
+export const getAssetHistory = (coin) => {
+  const now = new Date();
+  const end = now.getTime();
+  now.setDate(now.getDate() - 1);
+  const start = now.getTime();
+  return fetch(`${URL}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`)
+    .then(res => res.json())
+    .then(({ data }) => data);
+};
