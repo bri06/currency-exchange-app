0c46abdab0fbecb31adcc25f06f03cc9c1b91dd6
Briam <briammartinez@gmail.com>
2019-12-28 02:22:23 +0100

    Added vue-spinner ue-chartkick and chart.js to the project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue';
-import App from './App.vue';
+import Chartkick from 'vue-chartkick';
+import Chart from 'chart.js';
+import { VueSpinners } from '@saeris/vue-spinners';
 import '@/assets/css/tailwind.css';
+import App from './App.vue';
+
 
 import router from '@/routes';
 
 import { dollarFilter, percentFilter } from '@/utils';
+
+Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart));
+
 
 Vue.filter('dollar', dollarFilter);
 Vue.filter('percent', percentFilter);
