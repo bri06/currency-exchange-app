8cc266d310e111e2cb0df6c0981783887b68d2c5
Briam <briammartinez@gmail.com>
2019-12-30 14:20:20 +0100

    Added getMarkets and getExchange methods into the api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 const URL = 'https://api.coincap.io/v2';
+const MAX_MARKETS = 5;
 
 export const getAssets = () => fetch(`${URL}/assets?limit=20`)
   .then(res => res.json())
@@ -18,3 +19,11 @@ export const getAssetHistory = (coin) => {
     .then(res => res.json())
     .then(({ data }) => data);
 };
+
+export const getMarkets = id => fetch(`${URL}/assets/${id}/markets?limit=${MAX_MARKETS}`)
+  .then(res => res.json())
+  .then(({ data }) => data);
+
+export const getExchange = id => fetch(`${URL}/exchanges/${id}`)
+  .then(res => res.json())
+  .then(({ data }) => data);
